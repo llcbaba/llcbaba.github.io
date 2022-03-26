f679c3d12c354ad793618eb356c6b7c2953608f2
yakubu <seia.llc002@gmail.com>
2022-03-26 20:42:27 +0000

    new life

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/baba",
-  assetPrefix: "/baba",
+  basePath: "/",
+  assetPrefix: "/",
 };
 
 module.exports = nextConfig;
