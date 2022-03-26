1aae49e306b312200e523f28ab301bc1fc0509bb
yakubu <seia.llc002@gmail.com>
2022-03-26 01:33:51 +0000

    fix adding google ads

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3490157375757153"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </Head>
       <Component {...pageProps} />
