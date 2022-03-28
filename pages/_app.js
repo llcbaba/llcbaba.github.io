5f942fb1d5645c1fe9a5a61beb8efcc6b0a5ed7b
yakubu <seia.llc002@gmail.com>
2022-03-28 11:07:58 +0000

    added google add

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3490157375757153"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
