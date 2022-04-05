ac6cef97b93e90e1d7f3ccc86b860aca11f7ac88
yakubu <seia.llc002@gmail.com>
2022-04-05 06:23:56 +0000

    add adsterra ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <Script
+        <script
           type="text/javascript"
           src="//pl17169538.safestgatetocontent.com/86/b9/92/86b99238b4c0b87bd7e7a2642391327c.js"
-        ></Script>
+        ></script>
       </Head>
       <Component {...pageProps} />
     </Layout>
