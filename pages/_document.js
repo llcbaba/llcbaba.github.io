9717e79dd007ccd1089282962ca206a078f03148
yakubu <seia.llc002@gmail.com>
2022-04-05 09:28:52 +0000

    fixing ads

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          async="async"
+          data-cfasync="false"
+          src="//pl17169513.safestgatetocontent.com/5d84192ff9af6fbe751dc69ae3bb81fa/invoke.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="//pl17169538.safestgatetocontent.com/86/b9/92/86b99238b4c0b87bd7e7a2642391327c.js"
+        ></script>
+        <div id="container-5d84192ff9af6fbe751dc69ae3bb81fa"></div>
+      </body>
+    </Html>
+  );
+}
