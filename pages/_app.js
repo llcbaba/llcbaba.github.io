1ad051e30a3f140d10a1ba08f3bb611189f54431
yakubu <seia.llc002@gmail.com>
2022-04-05 06:02:24 +0000

    add adsterra ads

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,10 @@ function MyApp({ Component, pageProps }) {
           data-cfasync="false"
           src="//pl17169513.safestgatetocontent.com/5d84192ff9af6fbe751dc69ae3bb81fa/invoke.js"
         ></script>
+        <script
+          type="text/javascript"
+          src="//pl17169538.safestgatetocontent.com/86/b9/92/86b99238b4c0b87bd7e7a2642391327c.js"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </Layout>
