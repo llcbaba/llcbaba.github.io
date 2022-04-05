84504e33d5e0a19de3a2a9e60a0d622cfe0c3bba
yakubu <seia.llc002@gmail.com>
2022-04-05 06:25:19 +0000

    add adsterra ads

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,6 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <script
-          type="text/javascript"
-          src="//pl17169538.safestgatetocontent.com/86/b9/92/86b99238b4c0b87bd7e7a2642391327c.js"
-        ></script>
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
